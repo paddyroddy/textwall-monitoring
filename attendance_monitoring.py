614b22daff08a8f7203cecb5e2598f2b790ba815
paddyroddy <patrickjamesroddy@gmail.com>
2018-11-05 23:22:37 +0000

    split into 3 col until nrows=54

--- attendance_monitoring.py
+++ attendance_monitoring.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
 # read in csv
-df = pd.read_csv('textwall_sample.csv', nrows=10, header=None, names=['string', 'n/a', 'date', 'time'], parse_dates={'datetime': ['date', 'time']})
+df = pd.read_csv('textwall_sample.csv', nrows=53, header=None, names=[
+                 'string', 'n/a', 'date', 'time'], parse_dates={'datetime': ['date', 'time']})
 
 # remove email column
 df = df.drop('n/a', axis=1)
 
 # clean data
 df['string'] = df['string'].str.replace('?', ',')
-df['string'] = df['string'].str.lstrip(',')
+df['string'] = df['string'].str.replace(',', ' ')
 
 # split first column into three
-df = pd.concat([df['string'].str.split('\,|\s', expand=True), df['datetime']], axis=1)
+df = pd.concat([df['string'].str.split(expand=True), df['datetime']], axis=1)
 
 # set names of columns
 df.columns = ['student', 'course', 'phrase', 'datetime']
